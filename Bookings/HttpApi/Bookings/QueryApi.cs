e0a079d39b57dbb0f50d3bf84c6824c712ec9238
Ralph Jansen <ralph.jansen@outlook.com>
2024-06-20 13:58:37 +0200

    Add list API

--- Bookings/HttpApi/Bookings/QueryApi.cs
+++ Bookings/HttpApi/Bookings/QueryApi.cs
@@ -46,6 +46,39 @@ public class ProjectionsQueryApi : ControllerBase
     }
 
     [HttpGet]
+    public async Task<IActionResult> GetBooking(CancellationToken cancellationToken)
+    {
+        await using var connection = await ConnectionFactory.GetConnection(_connectionString, cancellationToken);
+
+        var cmd = connection.CreateCommand();
+        cmd.CommandText = $"SELECT * FROM {_schemaInfo.Schema}.bookings";
+        cmd.CommandType = CommandType.Text;
+
+        using (SqlDataReader reader = cmd.ExecuteReader())
+        {
+            List<BookingDocument> bookings = new();
+
+            while (reader.Read())
+            {
+                bookings.Add(new BookingDocument
+                {
+                    Id = reader["Id"].ToString(),
+                    GuestId = reader["GuestId"].ToString(),
+                    RoomId = reader["RoomId"].ToString(),
+                    CheckInDate = LocalDate.FromDateTime((DateTime)reader["CheckInDate"]),
+                    CheckOutDate = LocalDate.FromDateTime((DateTime)reader["CheckOutDate"]),
+                    BookingPrice = float.Parse(reader["BookingPrice"].ToString()),
+                    PaidAmount = float.Parse(reader["PaidAmount"].ToString()),
+                    Outstanding = float.Parse(reader["Outstanding"].ToString()),
+                    Paid = bool.Parse(reader["Paid"].ToString())
+                });
+            }
+
+            return Ok(bookings);
+        }
+    }
+
+    [HttpGet]
     [Route("{id}")]
     public async Task<IActionResult> GetBooking(string id, CancellationToken cancellationToken)
     {
